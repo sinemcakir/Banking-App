611c9ce9f13c70994fef142080a3fd682ec11306
sinemcakir <73499528+sinemcakir@users.noreply.github.com>
2024-11-25 22:21:04 +0300

    Fixed

--- BankingApp.API/Controllers/TransactionController.cs
+++ BankingApp.API/Controllers/TransactionController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BankingApp.API.Controllers
 {
 	[ApiController]
-	[Route("api/[controller]")] //Yorum
+	[Route("api/[controller]")] 
 	public class TransactionController : ControllerBase
 	{
 		private readonly ITransactionService _transactionService;
