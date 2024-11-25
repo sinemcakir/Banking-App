9233ac775a2d80b741fdf46c593b0dba55305331
sinemcakir <73499528+sinemcakir@users.noreply.github.com>
2024-11-25 22:19:35 +0300

    Yorum satırı eklendi

--- BankingApp.API/Controllers/TransactionController.cs
+++ BankingApp.API/Controllers/TransactionController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BankingApp.API.Controllers
 {
 	[ApiController]
-	[Route("api/[controller]")]
+	[Route("api/[controller]")] //Yorum
 	public class TransactionController : ControllerBase
 	{
 		private readonly ITransactionService _transactionService;
